cb87569b853c14ecd99556d9e39740cad5be6bbb
Brendan O'Brien <bobrien@pursuit.org>
2024-06-20 16:08:10 -0400

    gets app and server to talk to each other.

---
+++ app.js
@@ -0,0 +1,15 @@
+//DEPENDENCIES
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+
+// ROUTES
+app.get("/:verb/:adjective/:noun", (req, res) => {
+    const { verb, adjective, noun} = req.params;
+
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}\!`)
+})
+
+// EXPORT
+module.exports = app;
