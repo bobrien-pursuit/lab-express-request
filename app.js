cb40842644bb4e8be9f16358a02e2d96f6fe4664
Brendan O'Brien <bobrien@pursuit.org>
2024-06-20 19:34:25 -0400

    adds route with num parameter.

--- app.js
+++ app.js
@@ -12,10 +12,24 @@ app.get("/:verb/:adjective/:noun", (req, res) => {
 })
 
 // HOMEPAGE
-app.get("/bugs", (req, res) => {
+app.get("/bugs/", (req, res) => {
 
-    res.send(`99 little bugs in the code`);
-})
+    res.send(`99 little bugs in the code
+                <br>
+                <br>
+                <a href="/bugs/101">pull one down, patch it around</a>`)
+            
+            });
+app.get("/bugs/:num", (req, res) => {
+   const { num } = req.params;
+
+    res.send(`${num} little bugs left in the code
+                <br>
+                <br>
+                <a href="/bugs/${Number(num)}">pull one down, patch it around</a>`)     
+            
+            });
+
 
 // EXPORT
 module.exports = app;
